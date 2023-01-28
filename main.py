081e1f9b5c4367aa9d42f0106bf3a10472d7481b
Sentoss <34657765+Sentoss@users.noreply.github.com>
2023-01-28 11:34:08 +0200

    Add files via upload
    Just a few changes and added a comment detailing a probable solution to the current search function problem (inability to actually search aka. filter)

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ class LibraryManager(Gtk.Window):
                       'Booksinfo' : ['ISBN', 'Placement', 'Title', 'Author', 'Language', 'Available', 'Copies'],
                       'Searchfield' : ['ISBN', 'Placement', 'Title', 'Author', 'Language']}
         
-
         self.Issue()
         self.booksearch = Gtk.Entry()
         self.SearchCombo = Gtk.ComboBoxText()
@@ -34,7 +33,7 @@ class LibraryManager(Gtk.Window):
         self.Booksfilter.set_visible_func(self.filterchoice)
         self.fillData()
         self.dashboard = Gtk.TreeView(model=self.mainstore)
-        self.Booktable = Gtk.TreeView(model=self.Booksinfo)
+        self.Booktable = Gtk.TreeView(model=self.Booksfilter)
         self.setLabels()
         
         self.navigation = Gtk.Notebook()
@@ -46,18 +45,22 @@ class LibraryManager(Gtk.Window):
         self.navigation.append_page(self.Maintab, Gtk.Label(label="Main"))
         
         self.Bookstab = Gtk.Grid()
+        self.searchtest = Gtk.Button(label="search")
         
         self.Bookstab.attach(self.booksearch, 0, 0, 2, 2)
         self.Bookstab.attach_next_to(self.Booktable, self.booksearch, Gtk.PositionType.BOTTOM, 2, 2)
         self.Bookstab.attach_next_to(self.SearchCombo, self.booksearch, Gtk.PositionType.RIGHT, 2, 1)
+        self.Bookstab.attach_next_to(self.searchtest, self.SearchCombo, Gtk.PositionType.RIGHT, 2, 1)
         self.navigation.append_page(self.Bookstab, Gtk.Label(label="Books"))
 
+        self.searchtest.connect("clicked", self.update)
         self.Memberstab = Gtk.Grid()
         self.navigation.append_page(self.Memberstab, Gtk.Label(label="Members"))
 
         self.Settingstab = Gtk.Grid()
         self.navigation.append_page(self.Settingstab, Gtk.Label(label="Settings"))
-    
+        #self.booksearch.connect('activate', self.)
+        self.index = self.SearchCombo.get_active()
     def fillData(self):
         for x in Borrowed_Books:
             self.treeitr = self.mainstore.append(x)
@@ -95,13 +98,18 @@ class LibraryManager(Gtk.Window):
         
     def filterchoice(self, model, iter, data):
         if self.booksearch.get_text() != '':
-            field = self.SearchCombo.get_active_text()
-            index = self.SearchCombo.get_active()
-            model[iter][index] = field
-            
+            if str(model[iter][self.index]) not in self.booksearch.get_text():
+                return False
         else:
             return True
-
+        
+    #Considering that I'm tired, here's what you have to do wrt the search function and updating the thing
+    #you're going to create 2 functions: one which updates when the text changes, and one that updates when
+    #the combobox changes. And in both of them (I think) there's an input that gets sent to the 2nd argument  
+    #as an argument in that callable function (old error, got fixed), so we take that (which I believei is the
+    #the new state of the object (combobox or Entry). You have to use them each in their specific way and
+    #then use self.Booksinfo.refilter() inside both, and I believe this should work.
+    
 window = LibraryManager()
 window.connect('destroy', Gtk.main_quit)
 window.show_all()
