2138fc25c78c26bc06653cece736b69a3a568a65
Sentoss <34657765+Sentoss@users.noreply.github.com>
2023-01-29 04:01:05 +0200

    Add files via upload
    Added a few stuff incl. the GUI for the Issue/return tab. That's missing some functionality yet, but it doesn't look too bad. should be redesigned in the next version. well, we'll fix it in post.

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from gi.repository import Gtk
         # ['Les Mots et Les Choses', '1351351358'], ['The Order of things', '1351351258'],
          #['The Book Thief', '1351351234']]
         
-labels = ['Member', 'ID', 'Title', 'Placement', 'ISBN', 'Due Date', 'Copies Left']
 Books = Populate.populatebook()
 Members = Populate.populatemember()
 Borrowed_Books = []
@@ -24,6 +23,9 @@ class LibraryManager(Gtk.Window):
                       'Booksinfo' : ['ISBN', 'Placement', 'Title', 'Author', 'Language', 'Available', 'Copies'],
                       'Searchfield' : ['ISBN', 'Placement', 'Title', 'Author', 'Language']}
         
+        #a dict in which each key points to a list that contains all the iters of a store
+        self.ItersList = {'mainstore' : [],
+                          'Booksinfo' : []}
         self.Issue()
         self.booksearch = Gtk.Entry()
         self.SearchCombo = Gtk.ComboBoxText()
@@ -39,44 +41,82 @@ class LibraryManager(Gtk.Window):
         self.navigation = Gtk.Notebook()
         self.add(self.navigation)
         
+        #Grids
         self.Maintab = Gtk.Grid()
         self.Maintab.add(self.dashboard)
+        self.MainButtonsGrid = Gtk.Grid()
+        self.MainEntryGrid = Gtk.Grid()
         
+        #Entries
+        self.IssueLabel = Gtk.Label(label='Member ID')
+        self.IssueEntry = Gtk.Entry()
+        self.ReturnLabel = Gtk.Label(label='Book Placement')
+        self.ReturnEntry = Gtk.Entry()
+        self.MainEntryGrid.attach(self.IssueLabel, 0, 0, 1, 1)
+        self.MainEntryGrid.attach_next_to(self.IssueEntry, self.IssueLabel, Gtk.PositionType.RIGHT, 1, 1)
+        self.MainEntryGrid.attach(self.ReturnLabel, 0, 1, 1, 1)
+        self.MainEntryGrid.attach_next_to(self.ReturnEntry, self.ReturnLabel, Gtk.PositionType.RIGHT, 1, 1)
+        self.MainEntryGrid.set_row_spacing(20)
+        self.MainEntryGrid.set_column_spacing(10)
+        
+        #Buttons
+        self.IssueButton = Gtk.Button(label='Issue')
+        self.ReturnButton = Gtk.Button(label='Return')
+        self.MainButtonsGrid.attach(self.IssueButton, 0, 0, 1, 1)
+        self.MainButtonsGrid.attach_next_to(self.ReturnButton, self.IssueButton, Gtk.PositionType.BOTTOM, 1, 1)
+        
+        #Assimilation
+        self.Maintab.attach_next_to(self.MainButtonsGrid, self.dashboard, Gtk.PositionType.RIGHT, 2, 2)
+        self.Maintab.attach_next_to(self.MainEntryGrid, self.dashboard, Gtk.PositionType.TOP, 1, 1)
         self.navigation.append_page(self.Maintab, Gtk.Label(label="Main"))
         
+        ##Books Tab UI
+        #Grids       
         self.Bookstab = Gtk.Grid()
         self.ButtonGrid = Gtk.Grid()
         self.ContentGrid = Gtk.Grid()
+        
+        #Buttons
         self.Removebook = Gtk.Button(label='Remove')
+        
+        #Assimilation
         self.Bookstab.attach(self.ContentGrid, 0, 0, 2, 2)
         self.Bookstab.attach_next_to(self.ButtonGrid, self.ContentGrid, Gtk.PositionType.RIGHT, 1, 1)
-        
         self.ContentGrid.attach(self.booksearch, 0, 0, 2, 2)
         self.ContentGrid.attach_next_to(self.Booktable, self.booksearch, Gtk.PositionType.BOTTOM, 2, 2)
         self.ButtonGrid.attach(self.SearchCombo, 0, 0, 1, 1)
         self.ButtonGrid.attach_next_to(self.Removebook, self.SearchCombo, Gtk.PositionType.BOTTOM, 1, 1)
-         
         self.navigation.append_page(self.Bookstab, Gtk.Label(label="Books"))
 
-        self.booksearch.connect('activate', self.search)
-        self.Removebook.connect('clicked', self.removebook)
-        
+        # Members tab UI
+        # Grids
         self.Memberstab = Gtk.Grid()
+        
+        # Assimilation
         self.navigation.append_page(self.Memberstab, Gtk.Label(label="Members"))
 
+        # Settings tab UI
+        # Grids
         self.Settingstab = Gtk.Grid()
-        self.navigation.append_page(self.Settingstab, Gtk.Label(label="Settings"))
         
+        #Assimilation
+        self.navigation.append_page(self.Settingstab, Gtk.Label(label="Settings"))
 
+        # UI-Function connections
+        self.booksearch.connect('activate', self.search)
+        self.Removebook.connect('clicked', self.removebook)
+        self.IssueButton.connect('clicked', self.Issue)
+        self.ReturnButton.connect('clicked', self.Return)
+        
 
     def fillData(self):
         for x in Borrowed_Books:
-            self.treeitr = self.mainstore.append(x)
-        
+            treeitr = self.mainstore.append(x)
+            self.ItersList['mainstore'].append(treeitr)
         for y in Books:
-            self.treeitr = self.Booksinfo.append(y.getInfo())
+            treeitr = self.Booksinfo.append(y.getInfo())
+            self.ItersList['Booksinfo'].append(treeitr)
     
-    #a psuedo issuing function for now. Will be later edited to require a variable
     def Issue(self):
         for x in range(len(Members)):
             if random.choice([True, False]):
@@ -124,11 +164,18 @@ class LibraryManager(Gtk.Window):
         model, path = selected.get_selected_rows()
         if path is not None:
             iter = self.Booksinfo.get_iter(path)
-            self.Booksinfo.remove(iter)
-            
+            ISBN = self.Booksinfo[iter][0]
+            for x in self.ItersList['mainstore']:
+                if ISBN == self.mainstore[x][4]:
+                    self.mainstore.remove(x)
+                    self.ItersList['mainstore'].remove(x)
+                    break
+                
             for i in Books:
                 if i.ISBN == self.Booksinfo[iter][0]:
                     Books.remove(i)
+                    
+            self.Booksinfo.remove(iter)
             
 window = LibraryManager()
 window.connect('destroy', Gtk.main_quit)
